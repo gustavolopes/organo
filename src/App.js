f2b0dda27b5c00bdcd6310f375e204406e8725d9
Gustavo Lopes <gufarialopes@gmail.com>
2023-08-17 14:13:26 -0300

    Retirado o DEBUGGER

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
     //console.log(colaborador);
-    debugger
+    //debugger
     setColaboradores([...colaboradores, colaborador]);
   }
 
